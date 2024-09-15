4a32078d22610cff89847cca4926d277a6a99f90
varnit-mittal <Varnit.Mittal@iiitb.ac.in>
2024-09-15 22:38:05 +0530

    all things updated

---
+++ frontend/app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import { Home } from "@/components/component/home";
+
+export default function app() {
+  return (
+    <div>
+      <Home />
+    </div>
+    
+
+  );
+}
